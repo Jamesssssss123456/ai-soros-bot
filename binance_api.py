86ee33e0fcabc9ba016a2e59b6991ad2c31b81b4
Jamesssssss123456 <yap175028@gmail.com>
2025-05-02 18:45:30 +0800

    Add files via upload

---
+++ binance_api.py
@@ -0,0 +1,10 @@
+def fetch_all_symbols_data():
+    # 模擬資料
+    return {
+        "ALPACAUSDT": {
+            "oi_change_pct": 0.25,
+            "basis_percent_negative": 0.15,
+            "top_trader_account_ls_ratio": 1.2,
+            "top_trader_position_ls_ratio": 1.3
+        }
+    }

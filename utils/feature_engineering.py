ea805a401c4e1dfa12dfeb2e5a9106712f711367
Jamesssssss123456 <yap175028@gmail.com>
2025-05-03 11:47:42 +0800

    Delete utils/feature_engineering.py

--- utils/feature_engineering.py
+++
@@ -1,7 +0,0 @@
-def prepare_features(data_dict):
-    return [
-        data_dict["oi_change_pct"],
-        data_dict["basis_percent_negative"],
-        data_dict["top_trader_account_ls_ratio"],
-        data_dict["top_trader_position_ls_ratio"]
-    ]
